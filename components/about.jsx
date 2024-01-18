aca85513df29602d139a6eeb4a2899b9919146d5
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-01-18 13:33:55 +0800

    Update about and experience section

--- components/about.jsx
+++ components/about.jsx
@@ -20,29 +20,30 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className='mb-3'>
         After graduating with a degree in{' '}
-        <span className='font-medium'>Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{' '}
+        <span className='font-medium'>BS in Information Technology</span>, I
+        worked as an IT/Technical Support for three years. Subsequently, fueled
+        by my passion for programming, I decided to transition my career. To
+        refresh my skills and stay updated with the current tech trends, I
+        enrolled in a coding bootcamp where I delved into{' '}
         <span className='font-medium'>full-stack web development</span>.{' '}
-        <span className='italic'>My favorite part of programming</span> is the
-        problem-solving aspect. I <span className='underline'>love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{' '}
+        <span className='italic'>My favorite aspect of programming</span> is the
+        problem-solving challenge. I love the feeling of finally figuring out a
+        solution to a problem. I am particularly enthusiastic about
+        <span className='underline'></span> the constant learning process in the
+        dynamic field of technology. My core stack includes{' '}
         <span className='font-medium'>
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{' '}
-        <span className='font-medium'>full-time position</span> as a software
-        developer.
+        . Additionally, I am also familiar with TypeScript. My eagerness to
+        explore new technologies keeps me motivated. Currently, I am actively
+        seeking a <span className='font-medium'>full-time position</span> as a
+        software developer.
       </p>
 
       <p>
         <span className='italic'>When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and playing with my dog. I also
-        enjoy <span className='font-medium'>learning new things</span>. I am
-        currently learning about{' '}
-        <span className='font-medium'>history and philosophy</span>. I&apos;m
-        also learning how to play the guitar.
+        playing video games, watching movies, and playing with my dog/cats. I
+        also enjoy <span className='font-medium'>learning new things</span>.
       </p>
     </motion.section>
   )
