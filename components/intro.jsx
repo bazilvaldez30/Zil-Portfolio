1cd612b57100d8390f027229e6de2a8b7d78fd98
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-01-18 14:35:29 +0800

    Projects Card clickable - Redirect to the link of website

--- components/intro.jsx
+++ components/intro.jsx
@@ -37,11 +37,11 @@ export default function Intro() {
               height='192'
               quality='95'
               priority={true}
-              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+              className='h-24 w-24 rounded-full object-cover border-[0.35rem] dark:border-gray-600 border-white shadow-xl'
             />
           </motion.div>
 
-          <motion.span
+          {/* <motion.span
             className='absolute bottom-0 right-0 text-4xl'
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
@@ -53,7 +53,7 @@ export default function Intro() {
             }}
           >
             👋
-          </motion.span>
+          </motion.span> */}
         </div>
       </div>
 
@@ -66,8 +66,8 @@ export default function Intro() {
           Hello, I&apos;m John Bazil Milan Valdez.
         </span>{' '}
         I&apos;m a <span className='font-bold'>full-stack developer</span> with{' '}
-        <span className='font-bold'>1 year +</span> of experience. I enjoy
-        building <span className='italic'>sites & apps</span>. My focus is{' '}
+        <span className='font-bold'>years</span> of experience. I enjoy building{' '}
+        <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
       </motion.h1>
 
